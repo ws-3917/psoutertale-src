7357cc59de30a0e9ee17a1391a548234008854a4
WS-3917 <ws3917@outlook.com>
2025-04-08 15:58:58 +0800

    2025-04-08 15:58:56: V74.99 - Preview-6. PS! Outertale.

--- build.sh
+++ build.sh
@@ -54,8 +54,10 @@ if [ ! -d platforms ]; then yarn build-platforms; fi
 } &
 wait
 create_zip "linux-unpacked" "linux.zip" &
+create_zip "linux-arm64-unpacked" "linuxarm64.zip" &
 create_zip "win-unpacked" "win.zip" &
 create_zip "win-ia32-unpacked" "win7x32.zip" &
+create_zip "win-arm64-unpacked" "winarm64.zip" &
 wait
 
 echo build done
